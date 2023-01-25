3e0453bdea30bf6c298e854abff230f4fc19ff1e
KatarzynaBurdzy <katarzyna.anna.burdzy@gmail.com>
2023-01-25 14:23:00 +0100

    adding notes

--- components/App.jsx
+++ components/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
 
 function App() {
   return (
     <>
       <Header />
-      <Note />
+      {notes.map((x) => (
+        <Note key={x.key} title={x.title} content={x.content} />
+      ))}
       <Footer />
     </>
   );
